59f594861f51c0ec7233126fd10720b7a8540f3a
gyung688 <64251036+gyung688@users.noreply.github.com>
2022-10-14 00:45:55 +0900

    set project

---
+++ src/main/java/com/laon/model/BoardDto.java
@@ -0,0 +1,72 @@
+package com.laon.model;
+
+import java.util.Date;
+
+public class BoardDto {
+
+	private int boardNo;
+	private int cateNo;
+	private String title;
+	private String content;
+	private String regId;
+	private Date regDt;
+	private String updateId;
+	private Date updateDt;
+	private String delYn;
+	public int getBoardNo() {
+		return boardNo;
+	}
+	public void setBoardNo(int boardNo) {
+		this.boardNo = boardNo;
+	}
+	public int getCateNo() {
+		return cateNo;
+	}
+	public void setCateNo(int cateNo) {
+		this.cateNo = cateNo;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getRegId() {
+		return regId;
+	}
+	public void setRegId(String regId) {
+		this.regId = regId;
+	}
+	public Date getRegDt() {
+		return regDt;
+	}
+	public void setRegDt(Date regDt) {
+		this.regDt = regDt;
+	}
+	public String getUpdateId() {
+		return updateId;
+	}
+	public void setUpdateId(String updateId) {
+		this.updateId = updateId;
+	}
+	public Date getUpdateDt() {
+		return updateDt;
+	}
+	public void setUpdateDt(Date updateDt) {
+		this.updateDt = updateDt;
+	}
+	public String getDelYn() {
+		return delYn;
+	}
+	public void setDelYn(String delYn) {
+		this.delYn = delYn;
+	}
+	
+	
+}
